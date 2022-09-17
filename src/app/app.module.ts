2ecf2ace860627277a0d615b78c76928982c85c4
Carlos <carlosilvero52@gmail.com>
2022-09-16 23:08:51 -0300

    desafio componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ListaComponent } from './components/lista/lista.component';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { EstudiantesComponent } from './components/estudiantes/estudiantes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListaComponent,
+    EstudiantesComponent,
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap:  [ HeaderComponent, ListaComponent, EstudiantesComponent]
 })
 export class AppModule { }
